c847075d207feb49fa86151f77dfe7d4c985c545
saadkhalidkhan <ranasaad0799@gmail.com>
2018-09-27 10:41:22 +0500

    NearbyPlaces

---
+++ app/src/main/java/com/example/saadkhan/nearbyplaces/APIClient.java
@@ -0,0 +1,36 @@
+package com.example.saadkhan.nearbyplaces;
+
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+
+    private static Retrofit retrofit = null;
+
+    public static final String GOOGLE_PLACE_API_KEY = "AIzaSyA5xE7GkgwW1HeFs4obRlKGF6ZFIhYf3DE";
+
+    public static String base_url = "https://maps.googleapis.com/maps/api/";
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().readTimeout(30, TimeUnit.SECONDS).writeTimeout(30, TimeUnit.SECONDS).addInterceptor(interceptor).build();
+
+
+        retrofit = null;
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(base_url)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+        return retrofit;
+    }
+
+}
